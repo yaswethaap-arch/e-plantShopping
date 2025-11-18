8d9da345b9552623b56a7113b016632e74c034e8
yaswethaap-arch <ya.swethaap@gmail.com>
2025-11-18 11:57:41 +0530

    Update CartItem.jsx
    updated new code

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,23 +9,32 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach((item) =>{
+        total += parseFloat(item.cost.substring(1)) * item.quantity;
+    })
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({ name: item.name, quantity: item.quantity }));
   };
 
   // Calculate total cost based on quantity for an item
