ceda854ac08b668df3d5d7045ada952758fb33e3
yaswethaap-arch <ya.swethaap@gmail.com>
2025-11-18 22:37:42 +0530

    Update ProductList.jsx
    Added log messages

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -256,12 +256,14 @@ function ProductList({ onHomeClick }) {
     };
 
     const handleAddToCart = (product) => {
+        console.log("In handleAddToCart - Start")
         dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
       
         setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
           ...prevState, // Spread the previous state to retain existing entries
           [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
         }));
+        console.log("In handleAddToCart - End")
       };
     return (
         <div>
