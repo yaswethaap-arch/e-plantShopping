80cf338af16fb25fe0ad8f98ff99823d75d0b1b8
yaswethaap-arch <ya.swethaap@gmail.com>
2025-11-18 11:14:20 +0530

    Update ProductList.jsx
    Addedd removeItem and updateQuantity imports

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import {addItem} from './CartSlice';
+import {addItem, removeItem, updateQuantity} from './CartSlice';
 
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
