792052d9f100c46a41d90ab651683e76f7dfbad2
yaswethaap-arch <ya.swethaap@gmail.com>
2025-11-19 22:57:56 +0530

    Update ProductList.jsx
    Added cart total

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -259,15 +259,16 @@ function ProductList({ onHomeClick }) {
     };
 
     const handleAddToCart = (plant) => {
-        dispatch(addItem(plant)); // Dispatch the action to add the product to the cart (Redux action)
+        if(!(cartItems.find(item => item.name === plant.name))){
+            dispatch(addItem(plant)); // Dispatch the action to add the product to the cart (Redux action)
       
-        setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
-          ...prevState, // Spread the previous state to retain existing entries
-          [plant.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
-        }));
+            setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
+              ...prevState, // Spread the previous state to retain existing entries
+            [plant.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
+            }));
 
-        calculateTotalQuantity();
-        alert(plant.name+" has been added to cart.")
+            alert(plant.name+" has been added to cart.");
+        }
       };
 
       const calculateTotalQuantity = () => {
@@ -291,7 +292,7 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg><span className='badge'>{calculateTotalQuantity()>0?calculateTotalQuantity():''}</span></h1></a></div>
                 </div>
             </div>
             {!showCart ? (
@@ -312,7 +313,7 @@ function ProductList({ onHomeClick }) {
                                 <div className="product-title">{plant.name}</div> {/* Display plant name */}
                                 {/* Display other plant details like description and cost */}
                                 <div className="product-description">{plant.description}</div> {/* Display plant description */}
-                                <div className="product-cost">${plant.cost}</div> {/* Display plant cost */}
+                                <div className="product-cost">{plant.cost}</div> {/* Display plant cost */}
                                 <button
                                     className={cartItems.find(item => item.name === plant.name)? "product-button btn-disabled" : "product-button"}
                                     onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
