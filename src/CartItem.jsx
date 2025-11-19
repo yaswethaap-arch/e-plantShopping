7ffea12d8bb2a9d2808e95a96e9a984b4c049396
yaswethaap-arch <ya.swethaap@gmail.com>
2025-11-19 13:27:03 +0530

    Update CartItem.jsx
    Added code for total cost

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -39,6 +39,7 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return parseFloat(item.cost.substring(1)) * item.quantity;
   };
 
   return (
